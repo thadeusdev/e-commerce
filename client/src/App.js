34488b323a44e807f002a8f25776d1e6ec62bde0
thadeus <thadeusnyariki94@gmail.com>
2023-03-30 11:33:42 +0300

    layout

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,42 @@ import {
   RouterProvider,
   Outlet
 } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 import Home from './pages/Home/Home'
 import Products from './pages/Product/Product'
 import Product from "./pages/Product/Product";
 
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Layout />,
+    children: [ 
+      {
+        path: "/",
+        element: <Home />,
+      },     
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ]
   },
-  {
-    path: "/products/:id",
-    element: <Products />,
-  },
-  {
-    path: "/product/:id",
-    element: <Product />,
-  },
-])
+]);
 
 function App() {
   return (
