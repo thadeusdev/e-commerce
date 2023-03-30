a43c3381ccfa40d64a33f5e57e64deaea2a12568
thadeus <thadeusnyariki94@gmail.com>
2023-03-30 19:14:41 +0300

    featured products

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Slider from '../../components/Slider/Slider'
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
 import './Home.scss'
+import Categories from '../../components/Categories/Categories'
 
 const Home = () => {
   return (
     <div className='home'>
       <Slider />
       <FeaturedProducts type='featured' />
+      <Categories />
       <FeaturedProducts type='trending' />
     </div>
   )
